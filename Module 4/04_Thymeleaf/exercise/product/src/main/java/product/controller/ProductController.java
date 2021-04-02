7a1dc0358ee48e5a88af0fad858d13b45f0e560d
minhtuan94 <minhhoangtuan21@gmail.com>
2021-04-02 16:13:05 +0700

    02/04

--- Module 4/04_Thymeleaf/exercise/product/src/main/java/product/controller/ProductController.java
+++ Module 4/04_Thymeleaf/exercise/product/src/main/java/product/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import product.model.Product;
 import product.service.ProductService;
 
@@ -39,8 +40,8 @@ public class ProductController {
         return "/edit";
     }
 
-    @GetMapping("/product/{id}/delete")
-    public String delete(@PathVariable Integer id){
+    @GetMapping("/product/delete")
+    public String delete(@RequestParam Integer id){
         productService.delete(id);
         return "redirect:/";
     }
